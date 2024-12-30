5a34e4f1bbb1c9e47c8c7931f9e519ec77c06088
JAYANTH B S <jayanthbs004@gmail.com>
2024-12-30 10:08:05 +0530

    ..

--- bank-management/src/main/java/com/bankingmanagement/bank_management/config/SwaggerConfig.java
+++ bank-management/src/main/java/com/bankingmanagement/bank_management/config/SwaggerConfig.java
@@ -3,10 +3,7 @@ package com.bankingmanagement.bank_management.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,17 +11,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .info(new io.swagger.v3.oas.models.info.Info()
                         .title("API Documentation")
-                        .description("Spring Boot API with JWT and Swagger")
+                        .description("Spring Boot API")
                         .version("1.0"));
     }
 }
-
